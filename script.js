2baa852dea6c945f3a13410dc4e5b5b9c1d0f43b
Danya-web <danilameretov20@gmail.com>
2024-11-14 21:20:14 +0500

    edit Edited: script.js

--- script.js
+++ script.js
@@ -56,3 +56,55 @@ function addTask() {
     document.querySelector(".task-date").value = ""
     document.querySelector(".task-type").value = ""
 }
+
+loadTasks()
+
+function loadTasks() {
+    axios.get('http://localhost:3001/tasks')
+        .then(response => {
+            const tasks = response.data
+            Table(tasks)
+            Grid(tasks)
+        })
+}
+
+function Table(tasks) {
+    const tableBody = document.querySelector(".table-body")
+    tableBody.innerHTML = ''
+
+    tasks.forEach(task => {
+        const row = document.createElement("tr")
+        row.innerHTML = `
+            <td>${task.title}</td>
+            <td>${task.description}</td>
+            <td>${task.date}</td>
+            <td>${task.time}</td>
+            <td class="status ${task.status === "completed" ? "completed" : task.status === "in-progress" ? "in-progress" : "not-completed"}">
+                ${task.status === "completed" ? "Готово" : task.status === "in-progress" ? "В прогрессе" : "Не выполнено"}
+            </td>
+        `
+        tableBody.appendChild(row)
+    })
+}
+
+function Grid(tasks) {
+    const gridView = document.querySelector(".grid-view")
+    gridView.innerHTML = ''
+
+    tasks.forEach(task => {
+        const card = document.createElement("div")
+        card.className = "card"
+        card.innerHTML = `
+            <h3>${task.title}</h3>
+            <p class="desk">${task.description}</p>
+            <div class="data">
+                <p>${task.date}</p>
+                <p>${task.time}</p>
+            </div>
+            <p class="status ${task.status === "completed" ? "completed" : task.status === "in-progress" ? "in-progress" : "not-completed"}">
+                ${task.status === "completed" ? "Готово" : task.status === "in-progress" ? "В прогрессе" : "Не выполнено"}
+            </p>
+        `
+        gridView.appendChild(card)
+    })
+}
